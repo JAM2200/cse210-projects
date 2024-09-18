9f52a1ddcccb8d44a71bbed1f721006be4ac8b4a
JAM2200 <96632884+JAM2200@users.noreply.github.com>
2024-09-17 22:50:19 -0600

    Completed csharp prepares 1 and 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade (enter as %)? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        string letterGrade;
+
+        if(gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }else if(gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }else if(gradePercentage >= 70)
+        {
+            letterGrade = "C";
+
+        }else if(gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }else if(gradePercentage < 60)
+        {
+            letterGrade = "F";
+        }else{
+            letterGrade = " ";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+
+        if(gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }else
+        {
+            Console.WriteLine("Sorry, you did not pass the class. Better Luck next time.");
+        }
     }
 }
