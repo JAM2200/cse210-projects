2580a49060b5e6e0b1576558798128c1de2a0933
joseph_desktop <josephmaisey@gmail.com>
2024-09-18 14:04:31 -0600

    Finished Prep5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,40 @@ using System;
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    } 
+
+    static string PromptUserName()
+    {
+        Console.Write("Enter your user name: ");
+        return Console.ReadLine();
+    }
+     
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter your user number: ");
+        return int.Parse(Console.ReadLine());
+    } 
+
+
+    static int SquareNumber(int numberToSquare)
+    {
+        return numberToSquare * numberToSquare;
+    }
+
+    static void DisplayResult(string userName, int number){
+        Console.WriteLine($"{userName}, the square of your number is: {number}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
+
     }
 }
