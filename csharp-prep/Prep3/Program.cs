d3bb786404dc5ca8f75ad09c14ee5ca769774ef3
joseph_desktop <josephmaisey@gmail.com>
2024-09-18 12:39:10 -0600

    Finished the prep 3 assignment.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Drawing;
+using System.Globalization;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        string playAgain = "yes";
+        while(playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,101);
+
+            int numberOfGuesses = 0;
+            string response;
+
+            do
+            {
+
+                Console.Write("What is the magic number (enter q to quit or a number from 1 - 100)? ");
+                response = Console.ReadLine();
+                if(response == "q")
+                {
+                    continue;
+                }
+                int guess = int.Parse(response);
+                if(guess == magicNumber)
+                {
+                    Console.WriteLine("You guessed the magic number.");
+                    response = "q";
+                }else if(guess < magicNumber)
+                {
+                    Console.WriteLine("Guess a higher number.");
+                }else if(guess > magicNumber)
+                {
+                    Console.WriteLine("Guess a lower number.");
+                }
+
+                numberOfGuesses++;
+            }while(response != "q");
+
+            Console.WriteLine($"You made {numberOfGuesses} guesses.\n");
+            
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        }
+
     }
 }
