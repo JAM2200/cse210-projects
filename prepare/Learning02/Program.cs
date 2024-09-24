9c98d95610450bd7b3ee36423b520f186f5d0544
JAM2200 <96632884+JAM2200@users.noreply.github.com>
2024-09-24 09:13:18 -0600

    Finished Learning02 prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,49 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        // Instantiate a new job object;
+        Job job1 = new Job();
+
+        // Initialize its varibles with dot (.) notation.
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "2020";
+        job1._endYear = "2024";
+
+        // job1.Display();
+
+        // Console.WriteLine($"{job1._jobTitle}");
+
+        // Instaniate a second job object.
+        Job job2 = new Job();
+
+        // Initialize the variables with dot notation for later use.
+        job2._jobTitle = "Mechanical Engineer";
+        job2._company = "Chevy";
+        job2._startYear = "2014";
+        job2._endYear = "2021";
+
+        // Create a new resume object using the new Class() method;
+        Resume resume = new Resume();
+
+        // Add a name to the object.
+        resume._name = "John Doe";
+
+        // Fill in the resume with jobs worked.   
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        // Display the resume on the console.
+        resume.Display();
+
+        // Verify that individual job properties can be accesed through the resume object.  
+        // Commented out becuase they are not needed in the program. 
+        // Console.WriteLine($"{resume._jobs[0]._jobTitle}");
+        // Console.WriteLine($"{resume._jobs[1]._jobTitle}");
+      
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+    public Resume()
+    {
+    }
+
+    public void Display(){
+
+        Console.WriteLine($"Name: {_name}\nJobs:");
+        foreach(Job job in _jobs){
+                job.Display();
+        }
+    }
+}
