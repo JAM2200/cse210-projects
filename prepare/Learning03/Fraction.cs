b68696fc808fe9cf002b10707534066a7e9558ce
JAM2200 <96632884+JAM2200@users.noreply.github.com>
2024-09-30 21:54:01 -0600

    Completed Prepare 02.

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -5,48 +5,62 @@ class Fraction
     private int _bottomNumber;
     private int _topNumber;
 
+    // Create a public constructor that takes no arguments.
     public Fraction()
     {
+        // Set the top and bottom numbers equal to one to prevent unexpected behaviour.
         _topNumber = _bottomNumber = 1;
     }
 
+    // Create a public constructor that takes one argument.
     public Fraction(int wholeNumber)
     {
+        // Set the top number equal to the wholeNumber.
         _topNumber = wholeNumber;
+        // Set the bottom number equal to on so that it doesn't produce unexpected behaviour.
         _bottomNumber = 1;
     }
 
+    // Creat a third constructor that takes two arguments.
     public Fraction(int topNumber,int bottomNumber)
     {
+        // Set the top number equal to topNumber and the bottom number equal to bottomNumber.
         _topNumber = topNumber;
         _bottomNumber = bottomNumber;
     }
 
+    // A getter function to access the top number variable. 
     public int GetTop()
     {
         return _topNumber;
     }
+
+    // A getter function to access the bottom number variable. 
 
     public int GetBottom()
     {
         return _bottomNumber;
     }
 
+    // A setter function to change the top number variable.
     public void SetTop(int newTop)
     {
         _topNumber = newTop;
     }
 
+    // A setter function to change the bottom number variable.
     public void SetBottom(int newBottom)
     {
         _bottomNumber = newBottom;
     }
 
+    // A function to return the fraction in the form of "x/x."
     public string GetFractionString()
     {
         return _topNumber.ToString() + "/" + _bottomNumber.ToString(); 
     }
 
+    // A function to return the fraction as a decimal number.
     public double GetFractionDecimal()
     {
             return (double) _topNumber / (double)_bottomNumber;
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,22 +5,27 @@ class Program
     
     static void Main(string[] args)
     {
-
+        // Test each of the fraction constructors.
+        // No Argments.
         Fraction fraction1 = new Fraction();
+        // One argument for the numerator.
         Fraction fraction2 = new Fraction(7);
+        // Two Arguements, one for the numberator and the other for the denominator. 
         Fraction fraction3 = new Fraction(3,5);
 
+        // Display each fraction as a fractional number.
         Console.WriteLine($"fraction1 = {fraction1.GetFractionString()}");
         Console.WriteLine($"fraction2 = {fraction2.GetFractionString()}");
         Console.WriteLine($"fraction3 = {fraction3.GetFractionString()}");
 
+        // Verify that the setter functions work.
         fraction1.SetTop(5);
         fraction1.SetBottom(7);
-
+        
         Console.WriteLine($"fraction1 = {fraction1.GetFractionString()}");
         Console.WriteLine($"fraction1 = {fraction1.GetFractionDecimal()}");
 
-
+        // Test different numbers with the setter functions.
         fraction1.SetTop(3);
         fraction1.SetBottom(4);
 
@@ -32,6 +37,12 @@ class Program
 
         Console.WriteLine($"fraction1 = {fraction1.GetFractionString()}");
         Console.WriteLine($"fraction1 = {fraction1.GetFractionDecimal()}");
+
+
+        // Verify that the getter functions work.  Get the top and display it to the console.  Than get the bottom and display it to 
+        // the console. 
+        Console.WriteLine($"fraction1._topNumber = {fraction1.GetTop()}");
+        Console.WriteLine($"fraction1._bottomNumber = {fraction1.GetBottom()}");
 
     }
 }
